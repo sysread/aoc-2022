9099e8fa4cd93c293f8a5d8f7bcc5f54493a2f0c
Jeff Ober <sysread@fastmail.fm>
2022-12-01 15:11:01 -0700

    Day 1 solutions

---
+++ day_1a/solution.exs
@@ -0,0 +1,21 @@
+defmodule ElfCalorieCounter do
+  def scan(input_file) do
+    {_, max} = File.stream!(input_file) |> Enum.reduce({0, 0}, &scan_line/2)
+    max
+  end
+
+  defp scan_line("\n", {acc, max}) do
+    if acc > max do
+      {0, acc}
+    else
+      {0, max}
+    end
+  end
+
+  defp scan_line(line, {acc, max}) do
+    {num, _rem} = Integer.parse(line)
+    {acc + num, max}
+  end
+end
+
+ElfCalorieCounter.scan("input.txt") |> IO.puts()
