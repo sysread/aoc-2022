ec97ea738d7083f182b58f539dc9ed26f7916a10
Jeff Ober <sysread@fastmail.fm>
2022-12-06 16:28:38 -0700

    add elixir solutin for d6p1

---
+++ day6/solution1.exs
@@ -0,0 +1,30 @@
+#!/usr/bin/env elixir
+
+defmodule StartOfPacketScanner do
+  @sop_chars 4
+
+  def scan(input_file) do
+    input_file
+    |> File.stream!([], 1)
+    |> Enum.reduce_while({0, []}, &getch/2)
+  end
+
+  def getch(char, {idx, buf}) do
+    idx = idx + 1
+    buf = [char | buf]
+
+    if is_sop?(buf) do
+      {:halt, idx}
+    else
+      {:cont, {idx, buf}}
+    end
+  end
+
+  def is_sop?(buf) when length(buf) < @sop_chars, do: false
+
+  def is_sop?(buf) do
+    buf |> Enum.slice(0, @sop_chars) |> MapSet.new() |> MapSet.size() == @sop_chars
+  end
+end
+
+StartOfPacketScanner.scan("input.txt") |> IO.puts()
